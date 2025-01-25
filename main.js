088fecc3e7cec8164c1b4eee4515f4c984d8608f
LucasGuimil <l.d.guimil@gmail.com>
2025-01-24 21:51:01 -0300

    Agregando funciones y validaciones

--- main.js
+++ main.js
@@ -1,43 +1,59 @@
 //Simulador de préstamos
 
-function inicio () 
+function simular () 
     {
-    const nombre = prompt("Bienvenido a nuestro simulador de préstamos, ingrese su nombre para iniciar")
+    const nombre = prompt("Bienvenido a nuestro simulador de préstamos, ingrese su nombre para iniciar") ?? ""
     if(nombre == "") {
         alert("Debe ingresar un nombre válido")
         console.log(nombre)
         console.error("No ingresó un nombre válido")
     }else{ 
-        alert(`Bienvenido ${nombre}. Usted puede generar hasta 3 simulaciones de préstamo, en caso de que no quiera simular más de uno, puede tocar la tecla "Enter" para salir.`)
+        alert(`Bienvenido ${nombre}. Usted puede generar hasta 3 simulaciones de préstamo. Nuestro interés actual es del 40%. En caso de querer simular menos y salir del programa, ingrese cualquier carácter que no sea numérico.`)
         const prestamos = []
-        for (let cantidadSimulaciones = 1; cantidadSimulaciones<4; cantidadSimulaciones++) {
-            let montoSolicitado = parseInt(prompt("Ingrese el monto que desea solicitar, el mismo debe ser mayor a $ARS 1000 y menor que $ARS 5000000"))
-            console.log(montoSolicitado)
-            if(montoSolicitado=!undefined) {
-                if (montoSolicitado <= 1000 || montoSolicitado>= 5000000) {
+        for (let cantidadSimulaciones = 0; cantidadSimulaciones<3; cantidadSimulaciones++) { 
+            let montoSolicitado = parseInt(prompt(`Ingrese el monto que desea solicitar, el mismo debe ser mayor a $ARS 1000 y menor que $ARS 5000000.`))
+            if(isNaN(montoSolicitado)){
+                break
+                }else if (montoSolicitado <= 1000 || montoSolicitado>= 5000000) {
                 alert("El monto ingresado está fuera de los límites. Intente nuevamente.")
                 }else{
-                const cuotas = parseInt(prompt("Ingrese la cantidad de cuotas a pagar, el máximo a solicitar son 72 cuotas."))
-                if(cuotas>72){
-                    alert("La cantidad de cuotas ingresadas es mayor al límite. Intente nuevamente")
-                }else{
-                    prestamos.push(
-                        {
-                            monto: montoSolicitado,
-                            cantidadCuotas: cuotas,
-                            valorCuota: calculoCuotas()
-                        })
-                    console.log(prestamos)
+                    const cuotas = parseInt(prompt("Ingrese la cantidad de cuotas a pagar, el máximo a solicitar son 72 cuotas."))
+                    if(isNaN(cuotas)){
+                        console.error("Ha ingresado un dato no válido.")
+                        break
+                    } else if (cuotas>72){
+                        alert("La cantidad de cuotas ingresadas es mayor al límite. Intente nuevamente")
+                    }else{
+                        prestamos.push(
+                            {
+                                monto: montoSolicitado,
+                                cantidadCuotas: cuotas,
+                                valorCuota: calculoCuotas(montoSolicitado,cuotas)
+                            })
+                        if (cantidadSimulaciones==2){
+                            break
+                        } else{
+                        let proximoPrestamo = confirm("¿Desea simular otro préstamo?")
+                        if(proximoPrestamo==false) {
+                                break
+                            }
+                        }
+                    }
                 }
             }
-            
-
-        }
-        }
+            prestamos.length!=0 ? mostrarResultados(prestamos) : console.error("No hay préstamos simulados")
         }
     }
+
 function calculoCuotas(montoTotal,cantidadCuotas) {
     return ((montoTotal+(montoTotal*40/100))/cantidadCuotas)
 }
-inicio()
 
+function mostrarResultados (prestamos) {
+    console.log("Usted ha realizado las siguientes simulaciones.")
+    for(let i = 0; i<prestamos.length; i++) {
+        console.log(`Monto solicitado: ${prestamos[i].monto} - Cantidad de cuotas: ${prestamos[i].cantidadCuotas} - Valor de cuota: $ARS ${prestamos[i].valorCuota}`)
+    }
+}
+
+simular()
